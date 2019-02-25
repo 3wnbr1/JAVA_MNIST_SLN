7c3e8ff04c475e861d81e4b1fcc5e9ce0ddf4308
3wnbr1 <ewen.brun@icloud.com>
2019-02-25 17:53:49 +0100

    FileIO is complete and tested

--- src/backend/dataset/FileIO.java
+++ src/backend/dataset/FileIO.java
@@ -2,53 +2,63 @@ package backend.dataset;
 
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Stream;
 
 
 public class FileIO {
 	
-	private String name;
 	private File file;
 	private FileReader reader;
 	private FileWriter writer;
 	private BufferedReader readBuffer;
-	private BufferedWriter writeBuffer;
-	
-	public FileIO(String name, String path) {
-		this.name = name;
-		this.file = new File(path + name + ".dtst");
-	}
 	
 	/*
-	 * 
+	 * Construct a FileIO from a filepath
 	 */
 	public FileIO(String filepath) {
 		this.file = new File(filepath);
 	}
 	
 	/*
-	 * Write to a file
+	 * Private method to a file
 	 */
-	void write(String contents) throws IOException {
-		this.writer = new FileWriter(this.file);
+	private void write(String contents, boolean append) throws IOException {
+		if (!this.file.canWrite()) {
+			throw new IOException();
+		}
+		this.writer = new FileWriter(this.file, append);
 		this.writer.write(contents);
 		this.writer.close();
+	}
+	
+	/*
+	 * Write to a file
+	 */
+	public void write(String contents) throws IOException {
+		this.write(contents, false);
+	}
+	
+	/*
+	 * Append to a file
+	 */
+	public void append(String contents) throws IOException {
+		this.write(contents, true);
 	}
 	
 	/*
 	 * Read a file and return it
 	 */
 	List<String> readlines() throws IOException {
+		if (!this.file.canRead()) {
+			throw new IOException();
+		}
 		this.reader = new FileReader(this.file);
 		this.readBuffer = new BufferedReader(this.reader);
 		Stream<String> lines = this.readBuffer.lines();
@@ -63,4 +73,13 @@ public class FileIO {
 		return contents;
 	}
 	
+	/*
+	 * Delete File from FileSystem
+	 */
+	public void delete() throws IOException {
+		if (!this.file.canWrite()) {
+			throw new IOException();
+		}
+		this.file.delete();
+	}
 }
