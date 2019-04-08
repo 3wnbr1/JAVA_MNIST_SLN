8da6645026bea565e4a9881d8b8fed8545492e2a
3wnbr1 <ewen.brun@ecam.fr>
2019-04-08 16:08:47 +0200

    Cleanup

--- src/testing/SLNTest.java
+++ src/testing/SLNTest.java
@@ -1,19 +1,14 @@
 package testing;
 
-
-import java.util.Arrays;
-
 import org.junit.Test;
 
 import backend.dataset.Dataset;
-import backend.dataset.Image;
 import backend.models.SLN;
 
 public class SLNTest {
 	
 	private Dataset dataset;
 	private SLN sln;
-	private Image image;
 
 	@Test
 	public void test() {
@@ -23,7 +18,6 @@ public class SLNTest {
 		// Create SLN
 		this.sln = new SLN("SingleLayerNeuralNetwork v0.1", this.dataset);
 		
-		// this.sln.train(0.05, 15);
 		this.sln.train(0.0005, 150);
 	}
 
